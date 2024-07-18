68ef8da7373d342633a60b6e40dd10417cdcbddf
cwanko <165863686+cwanko@users.noreply.github.com>
2024-07-17 23:49:27 -0500

    started entering dependencies for project 1

---
+++ stats_data.py
@@ -0,0 +1,16 @@
+# Import dependencies 
+import pandas as pd
+import numpy as np
+import datetime as dt
+import matplotlib.pyplot as plt
+import scipy.stats as st
+import sklearn.datasets as dta
+from prophet import Prophet
+from pytrends.request import TrendReq 
+
+
+# Load the datasets 
+
+
+
+
